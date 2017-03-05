8b3f4941c5a735e3cb2468e5817fd17522f8dde4
任清阳 <qingyang.ren@mtime.com>
2017-03-05 16:14:46 +0800

    新建workSpace工程。

---
+++ VideoCall/TCVideoCallAdapterKit/TCVideoCallAdapterKit/TCVideoCallAdapterKit.h
@@ -0,0 +1,19 @@
+//
+//  TCVideoCallAdapterKit.h
+//  TCVideoCallAdapterKit
+//
+//  Created by 任清阳 on 2017/3/5.
+//  Copyright © 2017年 任清阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCVideoCallAdapterKit.
+FOUNDATION_EXPORT double TCVideoCallAdapterKitVersionNumber;
+
+//! Project version string for TCVideoCallAdapterKit.
+FOUNDATION_EXPORT const unsigned char TCVideoCallAdapterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCVideoCallAdapterKit/PublicHeader.h>
+
+
